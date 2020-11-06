66bb484e0926498dc2dcd62906b89c5d7993b396
Colin <colin.deproft@student.vinci.be>
2020-11-06 17:24:56 +0100

    Navmesh + Deplacement virus

--- Assets/Scripts/Virus.cs
+++ Assets/Scripts/Virus.cs
@@ -1,15 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Virus : MonoBehaviour
 {
 
     [SerializeField] private Transform target;
+    private NavMeshAgent _agent;
 
+    private void Awake()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetButton("Jump"))
+            MoveToTarget();
+    }
+    
     private void MoveToTarget()
     {
-        
+        _agent.SetDestination((target.position));
     }
 
     void Start()
@@ -17,9 +33,4 @@ public class Virus : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
