5f1bb75e5b997e4c91b4782c667fee66d2a1bc1e
Auristelle <49188724+Auristelle@users.noreply.github.com>
2020-11-07 21:49:32 +0100

    OnEntry

---
+++ Assets/Scripts/OnEntry/OnEntryLoadNextLevel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class OnEntryLoadNextLevel : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Player")
+            GameManager.GameManager.Instance.LoadScene(sceneName);
+    }
+}
