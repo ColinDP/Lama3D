8fe593112ccc51b3eeca684cc5663b69100aace5
Marc <meganckm@gmail.com>
2020-11-07 10:21:52 +0100

    Change main scene + SwapnerVirus.cs (add some items and coroutine)

--- Assets/Scripts/SpawnerVirus.cs
+++ Assets/Scripts/SpawnerVirus.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class SpawnerVirus : MonoBehaviour
 {
     [SerializeField] private GameObject virusObject;
-    private GameObject virusClone;
+    [SerializeField] private GameObject virusClone;
+    [SerializeField] private int numberOfViruses;
+    [SerializeField] private int timeBetweenInstantiate;
+
 
     private void Start()
     {
-        for (int i = 0; i < 3; i++)
+        StartCoroutine(InstantiateViruses());
+    }
+    
+    private IEnumerator InstantiateViruses()
+    {
+        for (int i = 0; i < numberOfViruses; i++)
         {
             virusClone = Instantiate(virusObject);
             virusClone.transform.position = transform.position;
-        }
+            yield return new WaitForSeconds(timeBetweenInstantiate);
+        }  
     }
 }
