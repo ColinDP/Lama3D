e0d1d7a586b565351e755098a83056261a395887
Marc <meganckm@gmail.com>
2020-11-07 11:32:58 +0100

    Change player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,22 +8,32 @@ public class Player : MonoBehaviour
 {
     private const float Speed = 5;
     private Rigidbody _rigidbody;
+    private Transform _transform;
     private Vector3 _currentVelocity;
-    
+
     private void Awake()
     {
         _rigidbody =  GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
     }
     private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         var hInput = Input.GetAxisRaw("Horizontal");
         var vInput = Input.GetAxisRaw("Vertical");
         var direction = new Vector3(-vInput, 0, hInput).normalized;
-
         _currentVelocity = Speed * direction;
         _currentVelocity.y = _rigidbody.velocity.y;
+        
+        //??
+        _transform.Rotate(new Vector3(0, hInput, 0) * 2f);
+        
     }
-    
+
     private void FixedUpdate()
     {
         _rigidbody.velocity = _currentVelocity;
